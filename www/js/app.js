57bb04255c0cae2a304bbf25c09ba12296f37ab8
Peter Grooby <peter.grooby@airways.co.nz>
2016-09-07 09:06:42 +1200

    First basic functionality

--- www/js/app.js
+++ www/js/app.js
@@ -50,21 +50,21 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.restaurants', {
+      url: '/restaurants',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'tab-restaurants': {
+          templateUrl: 'templates/tab-restaurants.html',
+          controller: 'RestaurantsCtrl'
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
+    .state('tab.restaurant-detail', {
+      url: '/restaurant/:restaurantId',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'tab-restaurants': {
+          templateUrl: 'templates/restaurant-detail.html',
+          controller: 'RestaurantDetailCtrl'
         }
       }
     })
--- www/js/services.js
+++ www/js/services.js
@@ -1,47 +1,29 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Restaurants', function($http) {
   // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    var Restaurants = {'restaurants' : []};
 
   return {
-    all: function() {
-      return chats;
+    all : function () {
+         return $http({
+            method: 'GET',
+            url: 'https://developers.zomato.com/api/v2.1/search',
+            headers: {'user_key' : '3d4e7b71dc47706408c4032e2b421570'},
+            params: {
+                entity_id: '90',
+                entity_type : 'city'
+            }
+        }).success(function (data) {
+            Restaurants = data;
+        });
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(restaurantId) {
+        console.log("get", Restaurants,Restaurants.restaurants.length);
+      for (var i = 0; i < Restaurants.restaurants.length; i++) {
+         if (Restaurants.restaurants[i].restaurant.id === restaurantId) {
+          return Restaurants.restaurants[i].restaurant;
         }
       }
       return null;
